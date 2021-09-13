c9bab272d2f01211c5c1acfb86d7684dfa4eabb7
DESKTOP-8NJ7U4F\ThanhNghiaPC <nguyenthanhnghia0907@gmail.com>
2021-09-13 17:54:30 +0700

    first commit

---
+++ Module01_OOP/Module01/Exam10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exam10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap h: ");
+            int h = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nhap m: ");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nhap s: ");
+            int s = int.Parse(Console.ReadLine());
+
+            int ts = ((s + 60) - 1) % 60;
+            int tm = 0;
+        }
+    }
+}
